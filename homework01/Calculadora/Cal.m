76c936d876702e411eac2f14279ebf6de51a4b17
Domingo Suarez Torres <domingo.suarez@gmail.com>
2010-01-18 16:49:13 -0600

    cleanup

---
+++ homework01/Calculadora/Cal.m
@@ -0,0 +1,14 @@
+//
+//  Cal.m
+//  Calculadora
+//
+//  Created by Domingo Suarez Torres on 18/01/10.
+//  Copyright 2010 SynergyJ. All rights reserved.
+//
+
+#import "Cal.h"
+
+
+@implementation Cal
+
+@end
---
+++ homework01/Calculadora/Calculator.h
@@ -0,0 +1,16 @@
+//
+//  Calculator.h
+//  Calculadora
+//
+//  Created by Domingo Suarez Torres on 18/01/10.
+//  Copyright 2010 SynergyJ. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Calculator : NSObject {
+
+}
+
+@end
